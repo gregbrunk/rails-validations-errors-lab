ef971bc7b94dd918a25457345c3a76dc160129c4
Greg Brunk <greg.brunk@gmail.com>
2016-07-27 15:24:32 -0600

    Finished homework

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,11 +1,13 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+  validates :first_name, presence: true, uniqueness: false, length: {maximum: 255}
+  validates :last_name, presence: true, uniqueness: false, length: {maximum: 255}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: /@/
+
 
   before_save :normalize_phone_number
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
-    # stretch
   end
 
 end
